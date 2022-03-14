09082737970a840360c5c9e2da35053b43febd70
[sungbumv] <sungbumv@gmail.com>
2022-03-14 15:42:40 +0900

    03.14 mon. 1. DB Connect(mysql) 스키마 연결 테스트 완료

---
+++ baekjoon/study_WPF/MySQL.cs
@@ -0,0 +1,37 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace study_WPF
+{
+    class MySQL
+    {
+        public MySQL(string serverName, string dataBase, string userId, string userPw)
+        {
+
+            MySqlConnection connection = new MySqlConnection("Server=" + serverName + ";Database=" + dataBase + ";Uid=" + userId + ";Pwd=" + userPw);
+
+            // DB Open
+            connection.Open();
+
+            //DB 연결여부 확인
+            Boolean pingFlag = connection.Ping();
+
+            //연결이 성공하였을 경우 
+            if(pingFlag == true)
+            {
+                MessageBox.Show("DB 연결 성공");
+            }
+            else
+            {
+                MessageBox.Show("DB 연결 실패");
+            }
+
+            connection.Close();
+        }
+    }
+}
